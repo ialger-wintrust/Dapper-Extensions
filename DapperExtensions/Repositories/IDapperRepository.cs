cef595ced7a40bcf5ce3fc74dd5a79816404c4b8
Ialger@wintrust.com <Ialger@wintrust.com>
2025-06-27 11:07:16 -0600

    Added Repository Layer interface so I am not constantly adding the same code over and over.

---
+++ DapperExtensions/Repositories/IDapperRepository.cs
@@ -0,0 +1,29 @@
+﻿using DapperExtensions.Predicate;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace DapperExtensions.Repositories;
+
+public interface IDapperRepository<T>
+{
+    Task<T?> Get(object id);
+
+    Task<IEnumerable<T>?> GetAll(Predicate<T>? predicate = null, IList<ISort>? sorts = null, int page = 1,
+        int resultsPerPage = 10, IDbTransaction? transaction = null);
+
+    Task<IEnumerable<T>?> Find(Predicate<T> predicate, IList<ISort>? sorts = null, IDbTransaction? transaction = null);
+
+    Task<dynamic> Add(T entity, IDbTransaction? transaction = null);
+
+    Task Add(IEnumerable<T> entities, IDbTransaction? transaction = null);
+
+    Task<int> Update(T entity, IDbTransaction? transaction = null);
+
+    Task<int> Update(IEnumerable<T> entities, IDbTransaction? transaction = null);
+
+    Task<bool> Delete(T entity, IDbTransaction? transaction = null);
+
+    Task<bool> Delete(IEnumerable<T> entities, IDbTransaction? transaction = null);
+}
